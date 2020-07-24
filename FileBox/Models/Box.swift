7eee0b741cc22415690c7708829af8d0a1ed5efa
Mikhail Lazarev <mikael.lazarev@gmail.com>
2020-07-24 12:52:20 +0300

    AppIcon + refactoring

---
+++ FileBox/Models/Box.swift
@@ -0,0 +1,16 @@
+//
+//  Box.swift
+//  FileBox
+//
+//  Created by Mikhail Lazarev on 24.07.2020.
+//  Copyright © 2020 Mikhail Lazarev. All rights reserved.
+//
+
+import Foundation
+
+struct Box {
+    var id: String
+    var name: String
+    var lat: Float
+    var lng: Float
+}
