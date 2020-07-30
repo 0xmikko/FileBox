bee8c79ca7ac45080847ce56e9cd1071eb4dffc9
Mikhail Lazarev <mikael.lazarev@gmail.com>
2020-07-30 12:57:29 +0300

    Interface embedding

---
+++ FileBox/Controllers/BoxesListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BoxListTableViewCell.swift
+//  FileBox
+//
+//  Created by Mikhail Lazarev on 30.07.2020.
+//  Copyright © 2020 Mikhail Lazarev. All rights reserved.
+//
+
+import UIKit
+
+class BoxListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FileBox/Controllers/BoxesListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BoxesListViewController.swift
+//  FileBox
+//
+//  Created by Mikhail Lazarev on 30.07.2020.
+//  Copyright © 2020 Mikhail Lazarev. All rights reserved.
+//
+
+import UIKit
+
+class BoxesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
